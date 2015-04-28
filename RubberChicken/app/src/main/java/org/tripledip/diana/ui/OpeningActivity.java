0589bb8dbcfb905fbd10625688ae6c1f42d8cce4
Ben Heasly <benjamin.heasly@gmail.com>
2015-04-27 22:15:57 -0400

    new opening activity with picture of diana sculpture

---
+++ RubberChicken/app/src/main/java/org/tripledip/diana/ui/OpeningActivity.java
@@ -0,0 +1,16 @@
+package org.tripledip.diana.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.tripledip.rubberchicken.R;
+
+public class OpeningActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opening);
+    }
+
+}
